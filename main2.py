0d7b94eeecbe86e30e48f5026ddce83add7c04a8
Emmanuel <emmanueljoeemie@gmail.com>
2024-08-02 23:53:18 +0100

    finish up

--- main2.py
+++ main2.py
@@ -7,6 +7,7 @@ filepaths = glob.glob("text/*.txt")
 # get the files
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
+#
 for filepath in filepaths:
     # print(filepath)
     filename = Path(filepath).stem
@@ -16,6 +17,11 @@ for filepath in filepaths:
     pdf.set_auto_page_break(auto=False, margin=0)
     pdf.add_page()
     pdf.set_font(family='Times', size=12, style='B')
-    pdf.cell(w=50, h=8, txt=f"{filename}")
+    pdf.cell(w=50, h=8, txt=f"{filename}",ln=1)
 
+    # read text file
+    with open(filepath, 'r') as file:
+        content = file.read()
+        # add content to pdf
+    pdf.multi_cell(w=0, h=6, txt=content)
 pdf.output("output.pdf")
